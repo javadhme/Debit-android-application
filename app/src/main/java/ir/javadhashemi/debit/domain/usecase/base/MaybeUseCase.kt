1f1b139702eee38ed03736bd53aa1ca09394e379
javadroid <javad.hashemi72@gmail.com>
2018-11-07 17:59:17 +0330

    create and modify base maybe, observable and single usecase

--- app/src/main/java/ir/javadhashemi/debit/domain/usecase/base/MaybeUseCase.kt
+++ app/src/main/java/ir/javadhashemi/debit/domain/usecase/base/MaybeUseCase.kt
@@ -1,15 +1,14 @@
 package ir.javadhashemi.debit.domain.usecase.base
 
-import io.reactivex.Observable
+import io.reactivex.Maybe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
-abstract class ObservableUseCase<R, T> : UseCase<R, Observable<T>>() {
+abstract class MaybeUseCase<R, T> : UseCase<R, Maybe<T>>() {
 
-    fun execute(
-            input : R? = null,
-            onResponse: (T) -> Unit
+    fun execute(input: R? = null,
+                onResponse: (T) -> Unit
     ): Disposable {
         return this
                 .execute(input)
--- app/src/main/java/ir/javadhashemi/debit/domain/usecase/base/SingleUseCase.kt
+++ app/src/main/java/ir/javadhashemi/debit/domain/usecase/base/SingleUseCase.kt
@@ -1,7 +1,28 @@
 package ir.javadhashemi.debit.domain.usecase.base
 
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
 /**
  * CREATED BY Javadroid FOR `Debit` PROJECT
  * AT: 2018/Nov/06 13:02
  */
-class SingleUseCase
+abstract class SingleUseCase<R, T> : UseCase<R, Single<T>>() {
+
+    fun execute(input: R?,
+                onResponse: (T) -> Unit
+    ): Disposable {
+        return this
+                .execute(input)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    onResponse(it)
+                }, {
+
+                })
+    }
+
+}
